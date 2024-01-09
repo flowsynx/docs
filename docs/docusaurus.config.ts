efcc612b0f057eca59d0085baccaf6abe844c8e4
ziagham <amin.ziagham@gmail.com>
2024-01-10 00:47:30 +0100

    change config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,16 +8,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://flowsynx.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Website',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'flowsynx', // Usually your GitHub org/user name.
+  projectName: 'Website', // Usually your repo name.
+  
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 

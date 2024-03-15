38960d0d863ab43d6a961765e8886d4593ac137c
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-03-15 21:46:46 +0100

    rename the website project name in config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'flowsynx', // Usually your GitHub org/user name.
-  projectName: 'Website', // Usually your repo name.
+  projectName: 'website', // Usually your repo name.
   
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -68,7 +68,7 @@ const config: Config = {
           position: 'left'
         },
         {
-          href: 'https://github.com/FlowSynx/FlowSynx',
+          href: 'https://github.com/flowsynx/flowsynx',
           label: 'GitHub',
           position: 'right',
         },
@@ -112,7 +112,7 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/FlowSynx/FlowSynx',
+              href: 'https://github.com/flowsynx/flowsynx',
             },
           ],
         },

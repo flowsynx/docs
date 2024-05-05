d480be2f37e6472341f8d5747d309dbfcc2c1e47
Ziagham Ahwazi, Amin <amin.ziagham@ge.com>
2024-05-05 15:32:04 +0200

    Modify the description regarding What is the FlowSynx?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,9 +45,9 @@ function HomepageAboutFlowSynx() {
         <Heading as="h2">What is the FlowSynx?</Heading>
         <div className="row">
           <div className="col">
-            FlowSynx is a program to manage files on cloud storage and locally. 
+            FlowSynx is a cross-platform program to manage files and data on cloud storage and locally. 
             It is a feature-rich alternative to cloud vendors' web storage interfaces. 
-            It will try to cover and support most of the cloud storage products including S3 object stores, 
+            It will try to cover and support most of the cloud storage products including Azure Files, Azure Blobs, 
             business & consumer file storage services, as well as standard transfer protocols.
           </div>
         </div>
